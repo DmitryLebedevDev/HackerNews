c275f587740323c0f9bca1a88facdb542450b3c7
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2019-11-28 19:59:06 +0800

    tree comments

--- src/redux/store-reducers.js
+++ src/redux/store-reducers.js
@@ -3,7 +3,7 @@ import { getElementById } from './../api/api';
 
 
 const ADD_STORY = 'ADD_TOP_STORY';
-
+const ADD_COMMENT_TO_STORY = 'ADD_COMMENT_TO_STORY'; 
 
 
 const addStory = (arrayStorys) => {
@@ -12,13 +12,60 @@ const addStory = (arrayStorys) => {
     storys: arrayStorys,
   }
 }
+const addStoryComment = (id,text) => {
+  return {
+    type: ADD_COMMENT_TO_STORY,
+
+  }
+}
+
+const addCommentToStory = (idStory) => (dispatch,story) => {
+
+}
+
 export const addTopStoryThunk = () => async (dispatch) => {
   return new Promise (async (res,req) => {
     let indexArrayStorys = await getTopStorys();
     let arrayStorys = [];
     console.log(indexArrayStorys);
-    for(let t = 0; t<10; t++) {
+    for(let t = 0; t<1; t++) {
+      async function Json (arr) {
+        if (arr===undefined||!arr.length) {
+          return
+        }
+        let json = {};
+        for (let t=0; t<arr.length; t++) {
+          let infoArrItem = await getElementById (arr[t]);
+          json[arr[t]] = {
+            id: arr[t],
+            name: infoArrItem.by,
+            text: infoArrItem.text,
+            comments: await Json(infoArrItem.kids)
+          }
+        }
+        return json
+      }
+      let info = await (Json([21656022]));
+      console.log(info);
       let infoStory = await getElementById(indexArrayStorys[t]);
+      /**
+       * {
+       *  id {
+*           name
+            text
+            comments {
+              id
+              id
+              id
+            }
+       *  }
+       *    
+       *    
+       *    comments
+       * }
+       */
+      let commentJson = {};
+      
       arrayStorys.push({
         id:indexArrayStorys[t],
         author: infoStory.by,
