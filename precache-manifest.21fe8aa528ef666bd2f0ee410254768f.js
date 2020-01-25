18f17e6615d3742d8ed076f81c0853568fcd0d28
gercog <54477063+Gercog52@users.noreply.github.com>
2020-01-25 17:49:46 +0800

    Updates

---
+++ precache-manifest.21fe8aa528ef666bd2f0ee410254768f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24f6e56b2e36461e0b0e36b4e083f9e",
+    "url": "/HackerNews/index.html"
+  },
+  {
+    "revision": "d9c4a3b7778973f04485",
+    "url": "/HackerNews/static/css/main.d17defc8.chunk.css"
+  },
+  {
+    "revision": "d488582e4b6c988a73e8",
+    "url": "/HackerNews/static/js/2.49b93375.chunk.js"
+  },
+  {
+    "revision": "d9c4a3b7778973f04485",
+    "url": "/HackerNews/static/js/main.54b8f4b8.chunk.js"
+  },
+  {
+    "revision": "05ed5f5f966da29b6914",
+    "url": "/HackerNews/static/js/runtime-main.a0114991.js"
+  }
+]);
