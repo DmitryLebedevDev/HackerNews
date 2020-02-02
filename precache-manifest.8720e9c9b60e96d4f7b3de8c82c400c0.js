87bcc9863dbfec92c68f1d3530753bd18f3a5ebd
gercog <54477063+Gercog52@users.noreply.github.com>
2020-02-02 22:32:01 +0800

    Updates

--- precache-manifest.8720e9c9b60e96d4f7b3de8c82c400c0.js
+++ precache-manifest.8720e9c9b60e96d4f7b3de8c82c400c0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "13e1e203c5ad6fde37d682a2823e7977",
+    "revision": "157faefc8d0d6174b1a0f2cd382f8a91",
     "url": "/HackerNews/index.html"
   },
   {
-    "revision": "5b210649e06a89178b1e",
+    "revision": "7e270e8725f7276f56ca",
     "url": "/HackerNews/static/css/main.ebec1cae.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HackerNews/static/js/2.9a3b467d.chunk.js"
   },
   {
-    "revision": "5b210649e06a89178b1e",
-    "url": "/HackerNews/static/js/main.f24fd8b9.chunk.js"
+    "revision": "7e270e8725f7276f56ca",
+    "url": "/HackerNews/static/js/main.fa503e71.chunk.js"
   },
   {
     "revision": "05ed5f5f966da29b6914",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HackerNews/precache-manifest.26909d1ca20ae6ac5c47ee6e2307fee5.js"
+  "/HackerNews/precache-manifest.8720e9c9b60e96d4f7b3de8c82c400c0.js"
 );
 
 self.addEventListener('message', (event) => {
