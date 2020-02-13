d00e9e809fc9258086056f9820549bb6ed5f6f70
gercog <54477063+Gercog52@users.noreply.github.com>
2020-02-13 19:19:45 +0800

    style name in User smol

--- src/components/userPage/UserSmol.tsx
+++ src/components/userPage/UserSmol.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { IUserLopped } from '../../redux/getByid-reducersType'
 import { egoDateToString } from '../../helpers/function'
+import styles from './UserPage.module.css';
+import { Link } from 'react-router-dom';
 
 export default function UserSmol(props:IUserLopped) {
   return (
-    <div>
+    <div className={styles.userSmol}>
       <div>
-        name: {props.id}
+        name: <Link className={styles.link} to={`/user/${props.id}`}>{props.id}</Link>
       </div>
       <div>
         created: {egoDateToString(props.created)}
